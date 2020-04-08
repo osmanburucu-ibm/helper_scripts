e340b69a7f9ecfd1703dc22d8a0e8638b110d89d
Osman Burucu - IBM <osman.burucu@at.ibm.com>
2020-04-08 09:37:58 +0200

    added flexible params , and virtjunkie repo info

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # helper_scripts
-simple scripts for my other projects
+simple scripts for my other projects, based on [Virtjunkie.com repo](https://github.com/jonhowe/Virtjunkie.com)
 
 ## Scripts
 
@@ -7,9 +7,11 @@ simple scripts for my other projects
 
 ~~~sh
 
-sh createvmclone.sh <vmpath> <newname> <basevmname> <ansible inventory file with path>
+sh createvmclone -v|--vms <path to vmware image directory> -n|--name <New VM name> -b|--base <Template/Base VM name> -i|--inventory <Ansible inventory file>
 
 ~~~
+
+The parameters -u|--user and -p|--pass are not used, but are the admin user for the vm image with its password
 
 prereqs: vmware tools have to be installed in the base image!
 
--- createvmclone.sh
+++ createvmclone.sh
@@ -1,50 +1,75 @@
 #!/bin/bash
 # this shell script will clone the given vmware fusion image with the new name
-# createvmclone <vmpath> <newname> <basevmname> <ansible inventory file with path>
-VMPATH="$1"
-NEWNAME="$2"
-BASEVMNAME="$3"
-INVENTORYFILE="$4"
+# createvmclone -v|--vms <path to vmware image directory> -n|--name <New VM name> -b|--base <Template/Base VM name> -i|--inventory <Ansible inventory file>
+# -u|--user and -p|--pass are not used, but are the admin user for the vm image with its password
+VMPATH=
+NEWNAME=
+BASEVMNAME=
+INVENTORYFILE=
+GUESTUSER=
+GUESTPASS=
 
-[ -z "$1" ] && VMPATH="~/VMs"
-[ -z "$2" ] && NEWNAME="cloned-image"
-[ -z "$3" ] && BASEVMNAME="COS77.base"
-[ -z "$4" ] && INVENTORYFILE="../hosts"
-
-
-BASEVMPATH="${BASEVMNAME}.vmwarevm"
-
-echo "using VMPATH=${VMPATH} NEWNAME=${NEWNAME} BASEVMNAME=${BASEVMNAME} BASEVMPATH=${BASEVMPATH} INVENTORYFILE=${INVENTORYFILE}"
-
-echo "will clone from base image"
-vmrun clone "${VMPATH}/${BASEVMPATH}/${BASEVMNAME}.vmx" "${VMPATH}/$NEWNAME.vmwarevm/$NEWNAME.vmx" full -cloneName=$NEWNAME
-
-echo "will start cloned image"
-vmrun start "${VMPATH}/$NEWNAME.vmwarevm/$NEWNAME.vmx"
-echo "need to wait till it is up"
-tstate=`vmrun checkToolsState "${VMPATH}/$NEWNAME.vmwarevm/$NEWNAME.vmx"`
-until [ $tstate = "running" ]
-do 
-    echo "checking state ..."
-    sleep 20
-    tstate=`vmrun checkToolsState "${VMPATH}/$NEWNAME.vmwarevm/$NEWNAME.vmx"`
+echo "Parameters are:"
+while true; do
+    case $1 in 
+        -u | --user )
+            GUESTUSER=$2; echo "User=$GUESTUSER"; shift 2 ;;
+        -p | --pass )
+            GUESTPASS=$2; echo "Pass=$GUESTPASS"; shift 2 ;;
+        -v | --vms )
+            VMPATH=$2; echo "VMs locations=$VMPATH"; shift 2 ;;            
+        -n | --name )
+            NEWNAME=$2; echo "New VM-Name=$NEWNAME"; shift 2 ;;
+        -b | --base )
+            BASEVMNAME=$2; echo "Base VM-Name=$BASEVMNAME"; shift 2 ;;
+        -i | --inventory )
+            INVENTORYFILE=$2; echo "Inventoryfile=$INVENTORYFILE"; shift 2 ;;
+        -- ) echo "-- $1"; shift; break ;;
+        * ) break ;;
+    esac
 done
-echo "state of vmtools = $tstate"
+echo
 
-echo "waiting for network to come up and get ip address..."
-ipaddresse=`vmrun getGuestIPAddress "${VMPATH}/$NEWNAME.vmwarevm/$NEWNAME.vmx"`
-until [[ $ipaddresse != *"Error"* ]]
-do 
-    echo "checking ip address ..."
-    sleep 20
-    ipaddresse=`vmrun getGuestIPAddress "${VMPATH}/$NEWNAME.vmwarevm/$NEWNAME.vmx"`
-done
-echo "ip address of vm is $ipaddresse"
+clone_vm() {
+    BASEVMPATH="${BASEVMNAME}.vmwarevm"
 
-echo "preparing inventory file for ansible"
-echo "[ucd_server]" > "$INVENTORYFILE"
-echo "$NEWNAME.local.net ansible_host=$ipaddresse" >> "$INVENTORYFILE"
-echo " " >> "$INVENTORYFILE"
-echo "[db_server]" >> "$INVENTORYFILE"
-echo "$NEWNAME.local.net ansible_host=$ipaddresse" >> "$INVENTORYFILE"
-echo "DONE"
+    SOURCE="${VMPATH}/${BASEVMPATH}/${BASEVMNAME}.vmx"
+    NEWVM="${VMPATH}/$NEWNAME.vmwarevm/$NEWNAME.vmx"
+
+    echo "CLONING: $BASEVMNAME to $NEWNAME"
+# vmrun -T fusion -gu $GUESTUSER -gp $GUESTPASS  ........
+    vmrun  clone $SOURCE $NEWVM full -cloneName=$NEWNAME 
+
+    echo "STARTING: $NEWVM"
+    vmrun start $NEWVM
+
+    echo "WAITING: .. starting .. vmtools .."
+    until (vmrun  checkToolsState $NEWVM | grep -q "running"); do 
+        printf '.'
+        sleep 5;
+    done
+    sleep 10
+    echo
+
+    echo "WAITING: .. vm-ip .. "
+    until (vmrun  getGuestIPAddress $NEWVM | grep -q -v "Error: Unable to get the IP address"); do
+        printf "."
+        sleep 5
+    done
+    sleep 10
+    echo
+
+    IP="$(vmrun  getGuestIPAddress $NEWVM)"
+    echo "VM is running and has the IP Address: $IP"
+    echo
+
+    echo "INVENTORYFILE: preparing inventory file for ansible"
+    echo "[ucd_server]" > "$INVENTORYFILE"
+    echo "$NEWNAME.local.net ansible_host=$IP" >> "$INVENTORYFILE"
+    echo " " >> "$INVENTORYFILE"
+    echo "[db_server]" >> "$INVENTORYFILE"
+    echo "$NEWNAME.local.net ansible_host=$IP" >> "$INVENTORYFILE"
+    echo
+    echo "DONE"
+}
+clone_vm $VMPATH $NEWNAME $BASEVMNAME $INVENTORYFILE $GUESTUSER $GUESTPASS
