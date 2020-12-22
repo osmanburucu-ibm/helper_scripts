0ca799fcf4ca3690aa22000edcb26fc0781306b0
Osman Burucu <osman.burucu@at.ibm.com>
2020-12-22 11:01:15 +0100

    added upgrade ucdagent script, fixed ssh-copy-id

--- vmware_automation/addtoknownhosts.sh
+++ vmware_automation/addtoknownhosts.sh
@@ -9,4 +9,6 @@ ssh-keygen -R $(dig +short $host)
 ssh-keyscan -t rsa $host | tee >> $HOME/.ssh/known_hosts
 ssh-keyscan $host | tee >> $HOME/.ssh/known_hosts
 
-ssh-copy-id -i ~/.ssh/id_rsa.pub $user@$host
+# ssh-copy-id -i ~/.ssh/id_rsa.pub $user@$host
+ssh-copy-id -i ~/.ssh/ansible-control-host.pub $user@$host
+
