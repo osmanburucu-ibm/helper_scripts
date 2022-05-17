4f47e0eac18a0f7f4d9f6dd32d0dab5e625b5029
Osman Burucu <osman.burucu@at.ibm.com>
2022-05-17 13:04:21 +0200

    exports and fixes

--- uc_dot_com_migration/src/dump_all_plugins.py
+++ uc_dot_com_migration/src/dump_all_plugins.py
@@ -21,10 +21,10 @@ fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-format=logging.Formatter("[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
+lformat=logging.Formatter("[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
 
-fh.setFormatter(format)
-ch.setFormatter(format)
+fh.setFormatter(lformat)
+ch.setFormatter(lformat)
 logger1 = logging.getLogger(script_name)
 
 
--- uc_dot_com_migration/src/uc_migration_utils.py
+++ uc_dot_com_migration/src/uc_migration_utils.py
@@ -18,10 +18,10 @@ fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-format=logging.Formatter("[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
+lformat=logging.Formatter("[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s")
 
-fh.setFormatter(format)
-ch.setFormatter(format)
+fh.setFormatter(lformat)
+ch.setFormatter(lformat)
 logger1 = logging.getLogger(__name__)
 
 
