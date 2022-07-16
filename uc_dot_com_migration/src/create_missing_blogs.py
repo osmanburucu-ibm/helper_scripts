2a16d4ca72817cec7f650d9d62e47a8d178d277c
Osman Burucu <osman.burucu@at.ibm.com>
2022-07-17 00:22:22 +0300

    added links from new blogs

--- uc_dot_com_migration/src/create_missing_blogs.py
+++ uc_dot_com_migration/src/create_missing_blogs.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 
 # create file handler which logs even debug messages
 fh = logging.FileHandler(f"{script_name}.log", "w+")
-fh.setLevel(logging.DEBUG)
+fh.setLevel(logging.INFO)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
@@ -58,18 +58,18 @@ def md(soup, **options):
 
 def copy_image_to_target(targetdir, image_name, blogs_dir, image_path):
     new_image_name = image_name
-    logger1.info(f"first new_image_name={new_image_name}")
+    logger1.debug(f"first new_image_name={new_image_name}")
     if not os.path.exists(f"{targetdir}/{new_image_name}"):
         if os.path.exists(f"{blogs_dir}/media/{image_path}/{new_image_name}"): 
             shutil.copyfile(f"{blogs_dir}/media/{image_path}/{new_image_name}", f"{targetdir}/{new_image_name}")
         else: 
-            logger1.info(f"image not found {new_image_name}")
+            logger1.debug(f"image not found {new_image_name}")
             if x := re.search("(-\d+?x\d+?\.png)", new_image_name):
-                logger1.info(f"image regex found {new_image_name}")
+                logger1.debug(f"image regex found {new_image_name}")
                 isplit = new_image_name.split(x[0])
                 iextension=Path(new_image_name).suffix
                 new_image_name=f"{isplit[0]}{iextension}"
-                logger1.info(f"new new_image_name={new_image_name}")
+                logger1.debug(f"new new_image_name={new_image_name}")
                 copy_image_to_target(targetdir, new_image_name, blogs_dir, image_path)
     return new_image_name
 
@@ -78,15 +78,16 @@ def get_publishing_date(ablog):
     d = datetime.strptime(ablog["pubDate"], '%a, %d %b %Y %H:%M:%S %z')
     return (d.strftime('%Y.%m.%d'))
 
-def replace_links_in_content (blog_content):
-    all_replacable_links = get_list_of_replacable_links()
+def replace_links_in_content (blog_content, all_replacable_links):
+    
     new_blog_content = blog_content
     for l in all_replacable_links:
-        if l:
+        logger1.info(f"l={l}")
+        if l:        
             new_blog_content = new_blog_content.replace(l[0], l[1])
     return new_blog_content
     
-def create_dir_and_files(ablog):
+def create_dir_and_files(ablog, all_replacable_links):
     config = ucutil.get_config()
     blogs_dir = config[ucutil.BLOGS_DIR]
     targetdir = f"{config[ucutil.BLOGS_DIR]}/migrated/{ablog['title']}"
@@ -106,10 +107,15 @@ def create_dir_and_files(ablog):
              
     d = get_publishing_date(ablog)
     
-    ablog_content = replace_links_in_content(ablog_content)
+    # # DEBUG
+    # if (ablog['link']=="https://www.urbancode.com/resource/creating-a-websphere-application-server-with-a-snippet/"):
+    #     logger1.info(f"DEBUG: OLD Content={ablog_content}")
+    ablog_content = replace_links_in_content(ablog_content, all_replacable_links)
+    # if (ablog['link']=="https://www.urbancode.com/resource/creating-a-websphere-application-server-with-a-snippet/"):
+    #     logger1.info(f"DEBUG: NEW Content={ablog_content}")
+    # TODO: check if URLs in content which are not in my list and add them!!!!
     
-    
-    new_content = f"<!DOCTYPE html>\n<html><head><title>{ablog['title']}</title></head>\n<body>\n<p><b>This article was originaly published in {d}</b></p>\n<p><h1>{ablog['title']}</h1></p>{ablog_content}\n</body>\n</html>\n"
+    new_content = f"<!DOCTYPE html>\n<html><head><title>{ablog['title']}</title></head>\n<body>\n<p><b>This article was originaly published in {d}</b></p>\n<p><h1>{ablog['title']}</h1></p>\n<p>{ablog_content}\n</p></body>\n</html>\n"
     with open(f"{targetdir}/content.html", "w") as afile:
         afile.write(new_content)
     
@@ -122,26 +128,26 @@ def create_dir_and_files(ablog):
 
 def process_all_images(blogs_dir, targetdir, ablogcontent, regex_image_type):
     ablog_content = ablogcontent
-    logger1.info(f"regex_imagetype={regex_image_type}")
+    logger1.debug(f"regex_imagetype={regex_image_type}")
     all_images=[]
     r = re.findall(rf"{regex_image_type}", ablog_content) 
 
     all_images.extend(r)
-    logger1.info(f"r={r}")
+    logger1.debug(f"r={r}")
     for ir in all_images:
         image_with_path=ir.strip()
         image_with_path=image_with_path.replace('http://www.urbancode.com', '')
         image_with_path=image_with_path.replace('href=', '')
         image_with_path=image_with_path.replace('src=', '')
         image_with_path=image_with_path.replace('"', '')
-        logger1.info(f"image link={image_with_path}")
+        logger1.debug(f"image link={image_with_path}")
         splitted = image_with_path.split("/")
-        logger1.info(f"{splitted}")
+        logger1.debug(f"{splitted}")
         image_name = splitted[-1]
 
-        logger1.info(f"Target={targetdir}/{image_name}")
+        logger1.debug(f"Target={targetdir}/{image_name}")
         image_path = Path(image_with_path).parent.absolute()
-        logger1.info(f"image_path={image_path}")
+        logger1.debug(f"image_path={image_path}")
         ablog_content=ablog_content.replace(f"{str(image_path)}/", "")
         ablog_content = re.sub(CAPTION_RE_START, "\n", ablog_content, flags=re.MULTILINE)
         ablog_content = re.sub(CAPTION_RE_END, "\n", ablog_content, flags=re.MULTILINE)
@@ -151,7 +157,7 @@ def process_all_images(blogs_dir, targetdir, ablogcontent, regex_image_type):
             ablog_content=ablog_content.replace(image_name, new_image_name)
     return ablog_content
         
-def create_blog(orig_link, my_dict):
+def create_blog(orig_link, my_dict, all_replacable_links):
     channel = my_dict['rss']['channel']
     ablog = {}
     for i in channel["item"]:
@@ -160,8 +166,8 @@ def create_blog(orig_link, my_dict):
             ablog["title"] = i['title']
             ablog["pubDate"]=i["pubDate"]
             ablog["content"]=i["content:encoded"]
-            logger1.info(f"Title={ablog['title']}")
-            create_dir_and_files(ablog)
+            logger1.debug(f"Title={ablog['title']}")
+            create_dir_and_files(ablog, all_replacable_links)
             
 def get_list_of_replacable_links():
     config = ucutil.get_config()
@@ -180,18 +186,33 @@ def get_list_of_replacable_links():
         for j in range(1, sh.max_column+1):
             cell_obj = sh.cell(row=i, column=j)
             print (f"{cell_obj.value} - ")
-            if j==1: orig_link=cell_obj.value
-            if (j==3 and str(cell_obj.value).lower() =="yes"):
+            if j==1: 
+                orig_link=cell_obj.value
+                continue
+            if j==2: continue
+            if (j==3):
+                if (str(cell_obj.value).lower() =="yes"):
+                    replace_link=cell_obj.value
+                    continue
+                else:
+                    orig_link = ""
+                    break
+            if (j==4 and replace_link.lower()=="yes"):
                 replace_link=cell_obj.value
                 break
-            else:
+            if (j > 4):
                 orig_link = ""
+                break
                 
         if orig_link: 
             logger1.info(f"Original-Link={orig_link} - Replace-Link={replace_link}")
             entry=[orig_link, replace_link]
             list_of_replacable_links.append(entry)
     wrkbk.close()
+    # add some additional replacements
+    list_of_replacable_links.append(['src="http://www.urbancode.comws.jpg"', 'src="ws.jpg"'])
+    list_of_replacable_links.append(['src="//www.youtube.com/embed/B1JpccSM3u8"', 'src="https://www.youtube.com/embed/hfftoBN9yXA"'])
+    
     return list_of_replacable_links
 
 def main():
@@ -201,6 +222,8 @@ def main():
     with open(f"{blogs_dir}/all_content.urbancode.WordPress.xml", "r") as xml_obj:
         my_dict = xmltodict.parse(xml_obj.read())
         xml_obj.close()
+    
+    all_replacable_links = get_list_of_replacable_links()
     
     # load excel with its path
     wrkbk = openpyxl.load_workbook(f"{blogs_dir}/MERGED-all_urls.xlsx")
@@ -214,14 +237,16 @@ def main():
         for j in range(1, sh.max_column+1):
             cell_obj = sh.cell(row=i, column=j)
             print (f"{cell_obj.value} - ")
-            if j==1: orig_link=cell_obj.value
-            if ("urbancode" not in orig_link) or (j==2 and cell_obj.value=="YES") or (j==3 and cell_obj.value!=None) or ("www.urbancode.com/plugin" in orig_link):
+            # if j==1: orig_link=cell_obj.value
+            # if ("urbancode" not in orig_link) or (j==2 and (cell_obj.value.upper)=="YES") or (j==3 and cell_obj.value!=None) or ("www.urbancode.com/plugin" in orig_link):
+            if j==1: orig_link=str(cell_obj.value)
+            if (j==3 and cell_obj.value!=None):
                 orig_link=""
                 break
                 
         if orig_link: 
             logger1.info(f"Original-Link={orig_link}")
-            create_blog(orig_link, my_dict)
+            create_blog(orig_link, my_dict, all_replacable_links)
             
     wrkbk.close() 
 if __name__ == '__main__':
